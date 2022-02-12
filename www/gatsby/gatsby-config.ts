4f9cae59962ba59f30f2b0e5c3c9fe05af4159e7
Khinshan Khan <khanshan501@gmail.com>
2022-02-12 18:24:41 -0500

    sanity gatsby packages to avoid path issues

--- www/gatsby/gatsby-config.ts
+++ www/gatsby/gatsby-config.ts
@@ -1,12 +1,22 @@
+import path from "path";
 import { GatsbyConfig } from "gatsby";
 
+const baseDir = path.join(__dirname, `..`);
+
 const plugins: GatsbyConfig["plugins"] = [
+  {
+    resolve: `gatsby-plugin-root-import`,
+    options: {
+      resolveModules: [baseDir],
+    },
+  },
   {
     resolve: `@chakra-ui/gatsby-plugin`,
     options: {
       resetCSS: true,
     },
   },
+  `gatsby-plugin-remove-trailing-slashes`,
 ];
 
 const config: GatsbyConfig = {
