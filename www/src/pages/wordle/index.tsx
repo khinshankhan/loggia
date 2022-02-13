d26310687d4826830b20cae94f596c92fcdd029a
Khinshan Khan <khanshan501@gmail.com>
2022-02-12 19:12:56 -0500

    create wordle board

--- www/src/pages/wordle/index.tsx
+++ www/src/pages/wordle/index.tsx
@@ -1,6 +1,32 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Heading } from "@chakra-ui/react";
 
-const Wordle = () => <Heading>Wordle!</Heading>;
+const Title = () => <Heading>Wordle</Heading>;
+
+const Board = () => (
+  <Container id="board">
+    <Grid templateColumns="repeat(1, 1fr)" gap={1}>
+      {[1, 2, 3, 4, 5, 6].map((guess) => (
+        <Grid id={`guess-${guess}`} key={`guess-${guess}`} templateColumns="repeat(6, 1fr)" gap={1}>
+          {[1, 2, 3, 4, 5, 6].map((letter) => (
+            <GridItem
+              id={`guess-${guess}-letter-${letter}`}
+              key={`guess-${guess}-letter-${letter}`}
+              w="100%"
+              h="20"
+              bg="papayawhip"
+            />
+          ))}
+        </Grid>
+      ))}
+    </Grid>
+  </Container>
+);
+
+const Wordle = () => (
+  <>
+    <Title /> <Board />
+  </>
+);
 
 export default Wordle;
