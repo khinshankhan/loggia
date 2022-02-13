84adc054e67f90443138e7546d974646b6c36dab
Khinshan Khan <khanshan501@gmail.com>
2022-02-12 21:54:15 -0500

    associate board to an array

--- www/src/pages/wordle/index.tsx
+++ www/src/pages/wordle/index.tsx
@@ -1,21 +1,50 @@
-import React from "react";
-import { Container, Grid, GridItem, Heading } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Center, Container, Grid, GridItem, Heading } from "@chakra-ui/react";
 
-const Title = () => <Heading>Wordle</Heading>;
+const Title = () => <Heading id="title">Wordle</Heading>;
 
-const Board = () => (
+const generateBoard2D = (r: number, c: number, k: string = ``) => {
+  const board = [];
+  for (let i = 0; i < r; i++) {
+    const row = [];
+    for (let j = 0; j < c; j++) {
+      row.push(k);
+    }
+    board.push(row);
+  }
+
+  return board;
+};
+
+interface IBoardProps {
+  board: string[][];
+  rowName?: string;
+  colName?: string;
+}
+
+const Board2D = ({ board, rowName = `row`, colName = `col` }: IBoardProps) => (
   <Container id="board">
     <Grid templateColumns="repeat(1, 1fr)" gap={1}>
-      {[1, 2, 3, 4, 5, 6].map((guess) => (
-        <Grid id={`guess-${guess}`} key={`guess-${guess}`} templateColumns="repeat(6, 1fr)" gap={1}>
-          {[1, 2, 3, 4, 5, 6].map((letter) => (
+      {board.map((row, rowIndex) => (
+        <Grid
+          id={`${rowName}-${rowIndex}`}
+          // eslint-disable-next-line react/no-array-index-key
+          key={`${rowName}-${rowIndex}`}
+          templateColumns={`repeat(${row.length}, 1fr)`}
+          gap={1}
+        >
+          {row.map((col, colIndex) => (
             <GridItem
-              id={`guess-${guess}-letter-${letter}`}
-              key={`guess-${guess}-letter-${letter}`}
+              id={`${colName}-${colIndex}`}
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${colName}-${colIndex}`}
               w="100%"
-              h="20"
               bg="papayawhip"
-            />
+              border="2px"
+              borderColor="black"
+            >
+              <Center h="20">{col}</Center>
+            </GridItem>
           ))}
         </Grid>
       ))}
@@ -23,10 +52,14 @@ const Board = () => (
   </Container>
 );
 
-const Wordle = () => (
-  <>
-    <Title /> <Board />
-  </>
-);
+const Wordle = () => {
+  const [guesses] = useState(generateBoard2D(6, 5));
+
+  return (
+    <div id="wordle">
+      <Title /> <Board2D board={guesses} />
+    </div>
+  );
+};
 
 export default Wordle;
